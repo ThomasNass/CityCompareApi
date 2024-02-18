c61c0fe10819f915419c212a38cda700b1fb611c
Thomas Näss <tn900323@gmail.com>
2024-02-18 14:25:16 +0100

    Made ScbResponses nullable

--- CityCompareProxy/Models/ScbResponse.cs
+++ CityCompareProxy/Models/ScbResponse.cs
@@ -2,29 +2,29 @@
 {
     public class ScbResponse
     {
-        public List<Column> Columns { get; set; }
-        public List<DataItem> Data { get; set; }
-        public List<MetadataItem> Metadata { get; set; }
+        public List<Column>? Columns { get; set; }
+        public List<DataItem>? Data { get; set; }
+        public List<MetadataItem>? Metadata { get; set; }
     }
 
     public class Column
     {
-        public string Code { get; set; }
-        public string Text { get; set; }
-        public string Type { get; set; }
+        public string? Code { get; set; }
+        public string? Text { get; set; }
+        public string? Type { get; set; }
     }
 
     public class DataItem
     {
-        public List<string> Key { get; set; }
-        public List<string> Values { get; set; }
+        public List<string>? Key { get; set; }
+        public List<string>? Values { get; set; }
     }
 
     public class MetadataItem
     {
-        public string Infofile { get; set; }
+        public string? Infofile { get; set; }
         public DateTime Updated { get; set; }
-        public string Label { get; set; }
-        public string Source { get; set; }
+        public string? Label { get; set; }
+        public string? Source { get; set; }
     }
 }
